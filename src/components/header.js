867e41f3b9602640b39a186dadbdfe7024ccac62
Ana Valeije <ana.valeije@nubank.com.br>
2018-03-24 18:37:29 -0300

    Adds style to Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
   return (
-    <Text style={textStyle}>"Albums!"</Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>"Albums!"</Text>
+    </View>
   );
 }
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
   textStyle: {
     fontSize: 20
   }
