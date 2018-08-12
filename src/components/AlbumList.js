ee4bd3975078be0c4b6a367cce339cae4f6f78a2
Ana Valeije <ana.valeije@gmail.com>
2018-08-12 00:43:51 -0300

    Renders album titles

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,16 +3,21 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends React.Component {
+  state = { albums: [] };
+
   componentDidMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
-    //console.log('TESTE');
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
   }
   
   render() {
     return (
       <View>
-        <Text>"Album List!!!"</Text>
+        {this.renderAlbums()}
       </View>
     );
   };
